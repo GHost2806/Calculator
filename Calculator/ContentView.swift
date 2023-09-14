f62b580c76630296bdde9a69afa9ed663bd5edd7
Macbook Air <kornilov.s2001@yandex.ru>
2023-09-14 15:34:31 +0500

    Initial commit

---
+++ Calculator/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Calculator
+//
+//  Created by Macbook Air on 14/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            ZStack {
+                color.black.edgesIgnoringSafeArea(.all)
+                
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
